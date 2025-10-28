43510279d123c98c896bdd4cc67b571ce1240800
anG1ous <105179521+anG1ous@users.noreply.github.com>
2025-10-28 13:01:06 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
         for (int i = 10; i < 15; i++) schedule[1][i] = true;
-        a.printPeriod(2);
+        for (int i = 30; i < 45; i++) schedule[1][i] = true;
+        for (int i = 50; i < 60; i++) schedule[1][i] = true;
+
         a.findFreeBlock(2, 15);
         AppointmentBook b = new AppointmentBook(schedule);
         int period = 2;
